42e2e270354aa061a65ac48d6637fbd0c45d4eb0
mesropt <mesroptnamak@gmail.com>
2021-08-31 19:14:46 +0300

    added tasks for lesson 5

---
+++ Урок 5. Введение в DOM/ПЗ /Mesrop_Homework_5_1.js
@@ -0,0 +1,13 @@
+/*
+1. Создать функцию, генерирующую шахматную доску. При этом можно
+использовать любые html-теги по своему желанию. Доска должна быть
+разлинована соответствующим образом, т.е. чередовать черные и белые
+ячейки. Строки должны нумероваться числами от 1 до 8, столбцы –
+латинскими буквами A, B, C, D, E, F, G, H. (использовать createElement /
+appendChild)
+ */
+
+document.body.insertAdjacentHTML('beforebegin', '<div><i>Hello!</i></div>');
+document.body.insertAdjacentHTML('afterbegin', '<div><i>Hello!</i></div>');
+document.body.insertAdjacentHTML('beforeend', '<div><i>Hello!</i></div>');
+document.body.insertAdjacentHTML('afterend', '<div><i>Hello!</i></div>');
